3b9f146b35aaf8e62f5abbd44e5c0fb88496ab5a
JordanBrainard <jordantbrainard@gmail.com>
2021-12-02 22:58:32 -0500

    day 2 part 2 solution

--- src/main/december/day2/PositionFinder.java
+++ src/main/december/day2/PositionFinder.java
@@ -9,17 +9,21 @@ public class PositionFinder {
     public Integer findEndpointProduct(List<Movement> movements) {
         int xDir = 0;
         int yDir = 0;
+        int aim = 0;
 
         for (Movement movement : movements) {
             switch (movement.getDirection()) {
                 case "forward":
                     xDir += movement.getDistance();
+                    yDir += aim * movement.getDistance();
                     break;
                 case "up":
-                    yDir -= movement.getDistance();
+                    aim -= movement.getDistance();
                     break;
                 case "down":
-                    yDir += movement.getDistance();
+                    aim += movement.getDistance();
+                    break;
+                default:
                     break;
             }
         }
